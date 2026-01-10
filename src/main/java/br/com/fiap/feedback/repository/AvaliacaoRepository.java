0e0be97ed2ee6be7e1491011c6362ea765f4bb2e
Julio Cesar Perroni <julio_veia@hotmail.com>
2026-01-09 21:58:34 -0300

    Refatoracao das pastas e do workflow

--- src/main/java/br/com/fiap/feedback/repository/AvaliacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.feedback.repository;
-
-import br.com.fiap.feedback.entity.AvaliacaoAula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvaliacaoRepository extends JpaRepository<AvaliacaoAula, Long> {
-}
